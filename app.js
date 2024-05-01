4a5425b9a9906a05bdb39b5d177669d1d3f89f96
FranciscoAlewaerts <franciscoalewaerts@gmail.com>
2024-05-01 12:54:34 -0300

    update 1.2

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+var form= document.getElementById("commentForm");
+
 
 function review(){
    location.href = "https://g.page/r/CZAZWX-EFxLwEBM/review" 
 }
 
 function comment(){
-   var form= document.getElementById("commentForm");
-   form.setAttribute("style","display:flex")
+form.setAttribute("style","display:flex")
 
+}
+
+function enviar(){
+   const message = document.getElementById("message");
+form.setAttribute("style", "display:none");
+message.textContent = "Gracias por su comentario!";
 }

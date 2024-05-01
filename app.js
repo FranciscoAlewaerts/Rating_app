28d649e4aad9b799bbe1cdc94aa14972dd0e6b45
FranciscoAlewaerts <franciscoalewaerts@gmail.com>
2024-05-01 12:19:09 -0300

    update 1.1

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 function review(){
    location.href = "https://g.page/r/CZAZWX-EFxLwEBM/review" 
 }
+
+function comment(){
+   var form= document.getElementById("commentForm");
+   form.setAttribute("style","display:flex")
+
+}
